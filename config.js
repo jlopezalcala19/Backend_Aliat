8eb986f59a12ae788ff90fe6e371583420a1dc5c
jlopezalcala19 <jlopezalcala1@hotmail.com>
2024-10-30 18:57:36 -0500

    actualiza base de datos aliat

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var config = {}
 config.puerto = 3000
-config.nombrebd = "Ingeser"
+config.nombrebd = "aliat"
 config.listablanca =[
     'http://localhost:4200'
 ]
 
 config.secretsession ="Nana17122019"
 config.tiemposession= (300000*2)
-config.namecookie = "ingesercookie"
+config.namecookie = "aliatcookie"
 module.exports.config = config
