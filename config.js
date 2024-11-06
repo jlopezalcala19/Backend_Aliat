5d2ddb63b087ce9b114bb88a0631666fd122ebe9
jlopezalcala19 <jlopezalcala1@hotmail.com>
2024-11-05 22:26:10 -0500

    Se actualiza nueva config de Cors

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {}
 config.puerto = process.env.PORT || 3000
 config.nombrebd = "aliat"
 config.listablanca =[
-    'https://aliatesp-frontend.netlify.app/',
+    'https://aliatesp-frontend.netlify.app',
     'http://localhost:4200'
 ]
 
