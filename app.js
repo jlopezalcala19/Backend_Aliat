4a45827f8605826a86983975939a1e102f749d82
jlopezalcala19 <jlopezalcala1@hotmail.com>
2024-11-05 21:17:23 -0500

    Se modifica cors allows headers

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.all('*', function(req, res, next) {
     var whitelist = req.headers.origin;
     res.header('Access-Control-Allow-Origin', whitelist);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,HEAD');
-    res.header('Access-Control-Allow-Headers', "*");// authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers
+    res.header('Access-Control-Allow-Headers', "authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     res.header("Access-Control-Allow-Credentials", "true");
     //res.header('Set-Cookie: cross-site-cookie=name; SameSite=None; Secure');
     next();
