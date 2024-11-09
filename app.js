84b18ec61c0cbc808f17e738a6fdb087c51c2aac
jlopezalcala19 <jlopezalcala1@hotmail.com>
2024-11-09 13:44:05 -0500

    Se corrige cookie  en linea de Cors

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ var session = require('express-session')({
     secret:config.secretsession,
     resave:true,
     saveUninitialized:true,
-    cookie:{path:'/', httpOnly:true, maxAge:config.tiemposession,sameSite:'lax', secure:true},
+    cookie:{path:'/', httpOnly:true, maxAge:config.tiemposession,sameSite:'none', secure:true},
     name:config.namecookie,
     rolling:true
  })
